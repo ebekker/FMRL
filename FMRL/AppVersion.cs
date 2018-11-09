d52d2f28291c0f99ab27ab4c2afd2bd45fb45b10
Eugene Bekker <ebekker@users.noreply.github.com>
2018-11-09 16:27:54 -0500

    adding version info to About

---
+++ FMRL/AppVersion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FMRL
+{
+    public static partial class AppVersion
+    {
+        public static string GetVersion()
+        {
+            string version = "(n/a)";
+            GetVersionInternal(ref version);
+            return version;
+        }
+
+        static partial void GetVersionInternal(ref string version);
+    }
+}
