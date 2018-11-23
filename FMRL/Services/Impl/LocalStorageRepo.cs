82f2b5817df89f8fc8378217b7dcc2ca18637557
Eugene Bekker <ebekker@users.noreply.github.com>
2018-11-22 19:28:21 -0500

    Adding repo impl using browser local storage
    * useful for local/disconnected development and testing

---
+++ FMRL/Services/Impl/LocalStorageRepo.cs
@@ -0,0 +1,47 @@
+﻿using Blazor.Extensions.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FMRL.Services.Impl
+{
+    /// <summary>
+    /// A repo implementation that stores all data in the browser's localStorage,
+    /// useful for testing or offline development.
+    /// </summary>
+    public class LocalStorageRepo : IRepo
+    {
+        LocalStorage _storage;
+
+        public LocalStorageRepo(LocalStorage storage)
+        {
+            _storage = storage;
+        }
+
+        public async Task Write(string type, string name, object value)
+        {
+            var path = $"{type}/{name}";
+            await _storage.SetItem(path, value);
+        }
+
+        public async Task<T> Write<T>(string type, string name, object value)
+        {
+            var path = $"{type}/{name}";
+            await _storage.SetItem(path, value);
+            return await _storage.GetItem<T>(path);
+        }
+
+        public async Task<T> Read<T>(string type, string name)
+        {
+            var path = $"{type}/{name}";
+            return await _storage.GetItem<T>(path);
+        }
+
+        public async Task Delete(string type, string name)
+        {
+            var path = $"{type}/{name}";
+            await _storage.RemoveItem(path);
+        }
+    }
+}
--- FMRL/Startup.cs
+++ FMRL/Startup.cs
@@ -1,3 +1,4 @@
+using Blazor.Extensions.Storage;
 using FMRL.Services;
 using FMRL.Services.Impl;
 using Microsoft.AspNetCore.Blazor.Builder;
@@ -12,8 +13,20 @@ namespace FMRL
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<ICrypto, BclCrypto>();
+            services.AddSingleton<BclCrypto>();
+
+#if USE_LOCAL_REPO
+            // Useful for local/disconnected development/testing;
+            // enable this by building with:
+            //    dotnet build /p:USE_LOCAL_REPO=1
+#warning Building with LOCAL REPO
+            services.AddStorage();
+            services.AddSingleton<IRepo, LocalStorageRepo>();
+#else
             //services.AddSingleton<IRepo, FirebaseRepo>();
             services.AddSingleton<IRepo, ParseRepo>();
+#endif
+
             services.AddToaster(config =>
             {
                 // Use the sample app to experiment with different options:
